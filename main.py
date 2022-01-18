cf326386717a929d2b377ca051f4c6827d84f4a8
Pynapple Dev #3 (ComputingSquid) <66024281+pureAWESOMNES0@users.noreply.github.com>
2022-01-18 19:10:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from time import sleep
+import os
+finished = 0
+
+while finished == 0:
+  days_survived = 0
+  print("_________________________________________")
+  print("|                                       |")
+  print("|          Text adventure game          |")
+  print("|           By: ComputingSquid          |")
+  print("|_______________________________________|")
+  sleep(2)
+  print('You have woken up, deciding to start a new life.\n')
+  sleep(1.5)
+  print("A new year means a new life \n")
+  sleep(1.5)
+  print("So you have decided to be a better person.\n")
+  sleep(1.5)
+  print("Different from the rutheless crime lord that you had used to be.\n")
+  sleep(1.5)
+  print("(In reality you were the proud owner of a daycare franchise.)\n")
+  sleep(1.5)
+  print("But let bygones be bygones.\n")
+  sleep(1.5)
+  print("Anyways...")
+  sleep(1.5)
+  print("Lets get around to the real stuff\n")
+  finished = 1
+
+print("You survived " + days_survived + "days")
